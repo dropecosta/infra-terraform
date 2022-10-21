353e101a5b64ffcdb7178bc75f6693e763eed518
Pedro Reis <dropecosta@gmail.com>
2022-10-21 02:48:06 +0100

    Refactoring Terraform main.tf

--- main.tf
+++ main.tf
@@ -1,7 +1,7 @@
 terraform {
   required_providers {
     digitalocean = {
-      source = "digitalocean/digitalocean"
+      source  = "digitalocean/digitalocean"
       version = "~> 2.0"
     }
   }
@@ -12,29 +12,41 @@ provider "digitalocean" {
   token = var.do_token
 }
 
-# Create a web server
-resource "digitalocean_kubernetes_cluster" "aula-iniciativa" {
-  name   = var.aula_name
+# Create a new Web Droplet in the nyc2 region
+resource "digitalocean_droplet" "jenkins" {
+  image    = "ubuntu-22-04-x64"
+  name     = var.name
+  region   = var.region
+  size     = "s-2vcpu-2gb"
+  ssh_keys = [data.digitalocean_ssh_key.jornada.id]
+}
+
+data "digitalocean_ssh_key" "jornada" {
+  name = "Jornada"
+}
+
+resource "digitalocean_kubernetes_cluster" "k8s" {
+  name   = "k8s"
   region = var.region
   # Grab the latest version slug from `doctl kubernetes options versions`
-  version = "1.23.9-do.0"
+  version = "1.24.4-do.0"
 
   node_pool {
     name       = "default"
     size       = "s-2vcpu-2gb"
-    node_count = 3
+    node_count = 2
   }
 }
 
 variable "do_token" {}
-variable "aula_name" {}
+variable "name" {}
 variable "region" {}
 
-output "kube_endpoint" {
-    value = digitalocean_kubernetes_cluster.aula-iniciativa.endpoint
+output "jenkins_ip" {
+  value = digitalocean_droplet.jenkins.ipv4_address
 }
 
-resource "local_file" "foo" {
-    content  = digitalocean_kubernetes_cluster.aula-iniciativa.kube_config.0.raw_config
-    filename = "kube=config.yaml"
+resource "local_file" "kube_config" {
+    content  = digitalocean_kubernetes_cluster.k8s.kube_config.0.raw_config
+    filename = "kube_config.yaml"
 }
--- terraform.tfvars
+++ terraform.tfvars
@@ -1,4 +1,4 @@
-do_token="dop_v1_9faef3bbd76b20ad204e00465ec12fa8a3a1e1568c2447c2f0e556768dd764d2"
-aula_name="aula-iniciativa"
-region="nyc1"
+do_token = "dop_v1_111e13012bbd9b91b80dcedd22c2a2b572a94b527a82faa76464125026b52c52"
+name     = "jenkins"
+region   = "nyc1"
 
